c218fd1b5c8c375d7cb7593225a82ee999674595
v-almonacid <ealmonacid@gmail.com>
2019-03-18 15:57:26 +0100

    added contracts/ directory included a chaincode MVP

---
+++ contracts/index.js
@@ -0,0 +1,10 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const Donations = require('./lib/donations');
+
+module.exports.Donations = Donations;
+module.exports.contracts = [ Donations ];
